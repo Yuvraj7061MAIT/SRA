1828b63e6fc5873c198cbfe606561f8784d0235e
Adeebahmed333 <adeebahmed3337@gmail.com>
2025-01-14 17:45:56 +0530

    Fixed the banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,25 +1,23 @@
-// src/components/Banner/Banner.jsx
-import React from "react";
-import "./Banner.css"; // Your custom styles
+import "./Banner.css";
 
 import banner1 from "../../../public/images/banner5.jpg";
 import banner2 from "../../../public/images/banner1.png";
-import banner3 from "../../../public/images//banner2.png";
-import banner4 from "../../../public/images//banner3.png";
+import banner3 from "../../../public/images/banner2.png";
+import banner4 from "../../../public/images/banner3.png";
 
 const Banner = () => {
-    return (
-        <section className="banner__section">
-            <div className="banner__slides">
- 
-                <img src={banner2} alt="banner2" />
-                <img src={banner3} alt="banner3" />
-                <img src={banner4} alt="banner4" />
-
-                <img src={banner1} alt="banner1" />
-            </div>
-        </section>
-    );
+  return (
+    <section className="banner__section">
+      <div className="banner__slides">
+        {/* Duplicating slides for circular effect */}
+        <img src={banner1} alt="Banner 1" />
+        <img src={banner2} alt="Banner 2" />
+        <img src={banner3} alt="Banner 3" />
+        <img src={banner4} alt="Banner 4" />
+        <img src={banner1} alt="Banner 1 Duplicate" />
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
